0af7335f9e8e5cdffdee9b9ba86a3aae6b29c1b9
JeffTheK <jeff144@protonmail.com>
2021-12-13 17:56:02 +0200

    Move install() into separate file

---
+++ src/install.py
@@ -0,0 +1,13 @@
+import os
+import shutil
+
+def install():
+    if os.path.isdir("Jeff-OS"):
+        print("Jeff-Os is already installed. Replacing installation")
+        shutil.rmtree("Jeff-OS")
+        print("finished")
+
+    print("starting installation")
+    os.mkdir("Jeff-OS")
+    os.mkdir("Jeff-OS/sys")
+    os.mkdir("Jeff-Os/usr")
--- src/main.py
+++ src/main.py
@@ -1,16 +1,5 @@
 import os
-import shutil
-
-def install():
-    if os.path.isdir("Jeff-OS"):
-        print("Jeff-Os is already installed. Replacing installation")
-        shutil.rmtree("Jeff-OS")
-        print("finished")
-
-    print("starting installation")
-    os.mkdir("Jeff-OS")
-    os.mkdir("Jeff-OS/sys")
-    os.mkdir("Jeff-Os/usr")
+from install import install
 
 def boot():
     if not os.path.isdir("Jeff-OS"):
