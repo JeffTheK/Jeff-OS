9fb99fd3c332b5024e568888b22897e86771cf67
JeffTheK <jeff144@protonmail.com>
2021-12-13 17:52:17 +0200

    Add basic booting and installing

---
+++ src/main.py
@@ -0,0 +1,18 @@
+import os
+import shutil
+
+def install():
+    if os.path.isdir("Jeff-OS"):
+        print("Jeff-Os is already installed. Replacing installation")
+        shutil.rmtree("Jeff-OS")
+        print("finished")
+
+    print("starting installation")
+    os.mkdir("Jeff-OS")
+    os.mkdir("Jeff-OS/sys")
+    os.mkdir("Jeff-Os/usr")
+
+def boot():
+    if not os.path.isdir("Jeff-OS"):
+        print("Jeff-OS is not installed")
+        install()
