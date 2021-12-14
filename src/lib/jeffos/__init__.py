84e055858f0f3a05cefd1ac669b89d82d1ad0373
JeffTheK <jeff144@protonmail.com>
2021-12-14 19:19:51 +0200

    Fix working dir again

--- src/lib/jeffos/__init__.py
+++ src/lib/jeffos/__init__.py
@@ -1,3 +1,24 @@
 import os
+from pathlib import Path
 
-OS_PATH = open("sys/var/sys.cfg").readlines()[0].strip()
+#OS_PATH = open("sys/var/sys.cfg").readlines()[0].strip()
+OS_PATH = "/home/dima/dev/Jeff-OS/Jeff-OS/" # FIXME: Ugly hack
+
+def getcwd():
+    cfg = open(OS_PATH+"sys/var/cmd.cfg", "r")
+    cwd = cfg.readlines()[0].strip()
+    return cwd
+
+def chdir(path):
+    cfg = open(OS_PATH+"sys/var/cmd.cfg", "r+")
+    lines = cfg.readlines()
+    cfg.close()
+    os.remove(OS_PATH+"sys/var/cmd.cfg")
+    cfg = open(OS_PATH+"sys/var/cmd.cfg", "w")
+    if path == "../":
+        path = Path(path)
+        lines[0] = str(path.parent.absolute())
+        print("new dir ", lines[0])
+    else:
+        lines[0] = os.path.join(lines[0]+ path)
+    cfg.writelines(lines)
