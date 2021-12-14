18e34a8f7f94b16431ffbdfde388a2b603e9cced
JeffTheK <jeff144@protonmail.com>
2021-12-15 01:53:50 +0200

    Restructure color

--- src/lib/jeffos/color.py
+++ src/lib/jeffos/color.py
@@ -1,13 +1,6 @@
 try:
-    import colorama
     from colorama import Fore, Back, Style
-    colorama.init()
-except ImportError:
-    print("colorama not installed, turning off color")
-
-
-def turn_off_color():
-    class DummyFore:
-        BLACK=RED=GREEN=YELLOW=BLUE=MAGENTA=CYAN=WHITE=RESET=''
-    
-    saved_Fore, Fore = Fore, DummyFore
+    from termcolor import colored
+except:
+    def colored(string, col1, col2=""):
+        return string
