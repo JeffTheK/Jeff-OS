535ea76d4bac36b3b969edcebd77c642cfcf361a
JeffTheK <jeff144@protonmail.com>
2021-12-14 02:19:52 +0200

    Fix OS_PATH

--- src/lib/jeffos/__init__.py
+++ src/lib/jeffos/__init__.py
@@ -1,15 +1,3 @@
 import os
 
-OS_PATH = open("sys/var/sys.cfg").readlines()[0]
-
-def getcwd():
-    path = open("sys/var/cmd.cfg").readlines()[0]
-    return path 
-
-def chdir(path):
-    file = open("sys/var/cmd.cfg", "r+")
-    lines = file.readlines()
-    lines[0] = path
-    os.remove("sys/var/cmd.cfg")
-    file = open("sys/var/cmd.cfg", "w")
-    file.writelines(lines)
+OS_PATH = open("sys/var/sys.cfg").readlines()[0].strip()
