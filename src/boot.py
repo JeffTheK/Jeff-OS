4a9896217ce8dcc3bb3080101297d2788f7b7d4d
JeffTheK <jeff144@protonmail.com>
2021-12-15 19:34:52 +0200

    Fix cmd path

--- src/boot.py
+++ src/boot.py
@@ -3,9 +3,10 @@ from pathlib import Path
 
 def boot():
     # run command line
-    our_path = str(Path(__file__).parent.absolute())
-    cmd_path = os.path.join(our_path, "../sys/bin/")
-    file = open("Jeff-OS/sys/bin/cmd")
+    our_path = Path(__file__).parent.absolute()
+    cmd_path = our_path.joinpath("sys/bin/cmd")
+    cmd_path = str(cmd_path)
+    file = open(cmd_path)
     exec(file.read(), globals())
 
 if __name__ == "__main__":
