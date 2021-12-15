8a1dbde24c9478b00aa1e8fa10e818cf65bb24f6
JeffTheK <jeff144@protonmail.com>
2021-12-16 01:23:06 +0200

    Add WARN and ERR to color

--- src/lib/jeffos/color.py
+++ src/lib/jeffos/color.py
@@ -2,7 +2,11 @@ try:
     from colorama import Fore, Back, Style
     from termcolor import colored
     OK = '['+colored("OK", "green")+']'
+    WARN = '['+colored("WARN", "yellow")+']'
+    ERR = '['+colored("ERR", "red")+']'
 except:
     def colored(string, col1, col2=""):
         return string
     OK = "[OK]"
+    WARN = "[WARN]"
+    ERR = "[ERR]"
