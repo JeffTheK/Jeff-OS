1c2a530cee3d7c19b873f0c12c29a458e779f80c
JeffTheK <jeff144@protonmail.com>
2021-12-16 01:30:37 +0200

    Import install.py with better messages + colors

--- install.py
+++ install.py
@@ -3,25 +3,26 @@ import shutil
 import sys
 from datetime import datetime
 from shutil import ignore_patterns
+from jeffos.color import colored, OK
 
 def install_default_apps():
-    print("installing default programs")
+    print(OK+"installing default programs")
     shutil.copytree("src/bin", "Jeff-OS/sys/bin", ignore=ignore_patterns('*.c', '*.h'))
 
 def install():
     if os.path.isdir("Jeff-OS"):
-        print("Jeff-OS is already installed. Replacing installation")
+        print(OK+"Jeff-OS is already installed. Removing old installation")
         shutil.rmtree("Jeff-OS")
-        print("finished")
+        print(OK+"finished")
 
-    print("starting installation")
+    print(OK+"starting installation")
     os.mkdir("Jeff-OS")
     os.mkdir("Jeff-OS/sys")
     os.mkdir("Jeff-OS/usr")
 
     install_default_apps()
     
-    print("installing libraries")
+    print(OK+"installing libraries")
     cfg = open("src/lib/jeffos/jeffos.cfg.py", "x")
     cfg.write(os.getcwd() + "/Jeff-OS")
     cfg.close()
@@ -29,8 +30,10 @@ def install():
     os.system("cd Jeff-OS/sys/lib/ && pip install .")
     os.remove("src/lib/jeffos/jeffos.cfg.py")
 
-    print("installing vars")
+    print(OK+"installing vars")
     os.mkdir("Jeff-OS/sys/var")
+    # sys.cfg
+    print(OK+"setting sys.cfg")
     os_name = "Jeff-OS"
     build_time = datetime.now()
     build_time = build_time.strftime("%Y-%m-%d %H:%M")
@@ -40,17 +43,19 @@ def install():
     sys_cfg.write(os_name + "\n")
     sys_cfg.write(build_time + "\n")
     sys_cfg.close()
-    # user
+    # usr.cfg
     usr_cfg = open("Jeff-OS/sys/var/usr.cfg", 'w')
     usr_cfg.write("NO_NAME \n")
     usr_cfg.write("NO_PASSWORD \n")
     usr_cfg.close()
 
+    # cmd.cfg
     cmd_cfg = open("Jeff-OS/sys/var/cmd.cfg", 'w')
     cmd_cfg.write(actual_os_dir)
     cmd_cfg.close()
 
     # booting and restarting
+    print(OK+"copying booting files")
     shutil.copy("src/boot.py", "Jeff-OS/boot.py")
 
 if __name__ == "__main__":
