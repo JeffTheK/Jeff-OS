d612da947805897cde2ce388c7db008603389a04
JeffTheK <jeff144@protonmail.com>
2021-12-16 01:17:24 +0200

    Add OK global in color

--- src/lib/jeffos/color.py
+++ src/lib/jeffos/color.py
@@ -1,6 +1,8 @@
 try:
     from colorama import Fore, Back, Style
     from termcolor import colored
+    OK = '['+colored("OK", "green")+']'
 except:
     def colored(string, col1, col2=""):
         return string
+    OK = ""
