b37909485b8ecf01767f9297fe2f057459be03b7
JeffTheK <jeff144@protonmail.com>
2021-12-16 18:31:15 +0200

    Add current user

--- src/boot.py
+++ src/boot.py
@@ -16,6 +16,7 @@ def boot():
     usr_cfg = open(jeffos.OS_PATH+"sys/var/usr.cfg", 'r+')
     if usr_cfg.read() == "":
         print(OK+"no users present, adding root user")
+        usr_cfg.write("root\n") # set current user
         jeffos.run_system_app("useradd", "root")
     usr_cfg.close()
 
